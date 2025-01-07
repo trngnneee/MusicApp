56f56d44b32acc7cbf5623d971412716f5b35157
trngnneee <dtnguyen23@clc.fitus.edu.vn>
2025-01-07 14:55:29 +0700

    Added functions for password input

--- src/app/(pages)/(auth)/login/LoginForm.tsx
+++ src/app/(pages)/(auth)/login/LoginForm.tsx
@@ -6,21 +6,46 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import Swal from 'sweetalert2/dist/sweetalert2.js'
 import 'sweetalert2/src/sweetalert2.scss'
+import { FaRegEye } from "react-icons/fa";
+import { FaRegEyeSlash } from "react-icons/fa";
+import { useState } from "react";
 
 export const LoginForm = () => {
     const router = useRouter();
+    const [view, setView] = useState(false);
 
-    const handleLogin = (event: any) => {
+    const handleLogin = async (event: any) => {
         event.preventDefault();
         const email = event.target.email.value
         const password = event.target.password.value;
-        signInWithEmailAndPassword(authFireBase, email, password).then(() => {
-            router.push("/");
+        if (!email || !password)
+        {
             Swal.fire({
-                title: "Đăng nhập thành công!",
-                icon: "success",
+                title: "Vui Lòng Nhập Đủ Thông Tin!",
+                icon: "error",
             });
-        })
+        }
+        else
+        {
+            try {
+                await signInWithEmailAndPassword(authFireBase, email, password);
+                router.push("/");
+                Swal.fire({
+                    title: "Đăng nhập thành công!",
+                    icon: "success",
+                });
+            } catch (error) {
+                Swal.fire({
+                    title: "Thông Tin Đăng Nhập Không Chính Xác!",
+                    icon: "error",
+                });
+            }
+        }
+    }
+
+    const handleViewPassword = (event:any) => {
+        event.preventDefault();
+        setView(!view);
     }
 
     return (
@@ -50,12 +75,20 @@ export const LoginForm = () => {
                         <div className="text-[14px] font-[600] text-white">Mật Khẩu</div>
                         <div className="text-[red]">*</div>
                     </label>
-                    <input
-                        type="password"
-                        name="password"
-                        id="password"
-                        className="px-[16px] py-[14px] rounded-[6px] outline-none w-[500px]"
-                    />
+                    <div className="flex w-[500px] px-[16px] py-[14px] rounded-[6px] bg-white">
+                        <input
+                            type={view ? "text" : "password"}
+                            name="password"
+                            id="password"
+                            className="outline-none w-full"
+                        />
+                        <button 
+                            type="button"
+                            onClick={handleViewPassword}
+                        >
+                            {view ? <FaRegEyeSlash /> : <FaRegEye />}
+                        </button>
+                    </div>
                 </div>
                 <button className="bg-[#00ADEF] w-[500px] text-[white] text-[16px] font-[700] rounded-[6px] py-[14px]">Đăng nhập</button>
                 <div className="mt-[10px] flex text-white gap-[8px] justify-end items-center">
--- src/app/(pages)/(auth)/register/FormRegister.tsx
+++ src/app/(pages)/(auth)/register/FormRegister.tsx
@@ -5,18 +5,45 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { ref, set } from "firebase/database";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import Swal from 'sweetalert2/dist/sweetalert2.js'
 import 'sweetalert2/src/sweetalert2.scss'
+import { FaRegEye } from "react-icons/fa";
+import { FaRegEyeSlash } from "react-icons/fa";
 
 export const RegisterForm = () => {
     const router = useRouter();
+    const [view, setView] = useState(false);
+    const [view2, setView2] = useState(false);
+
     const handleSubmit = (event: any) => {
         event.preventDefault();
         const fullName = event.target.fullName.value;
         const email = event.target.email.value
         const password = event.target.password.value;
+        const confirmPass = event.target.confirmPass.value;
 
-        if (fullName && email && password) {
+        if (!fullName || !email || !password || !confirmPass)
+        {
+            Swal.fire({
+                title: "Vui Lòng Điền Đủ Thông Tin!",
+                icon: "error",
+            });
+        }
+        else if (password.length < 7)
+        {
+            Swal.fire({
+                title: "Độ dài mật khẩu phải lớn hơn 7 kí tự!",
+                icon: "error",
+            });
+        }
+        else if (password != confirmPass) {
+            Swal.fire({
+                title: "Mật Khẩu Không Trùng!",
+                icon: "error",
+            });
+        }
+        else if (fullName && email && password && password == confirmPass) {
             createUserWithEmailAndPassword(authFireBase, email, password)
                 .then((userCredential) => {
                     const user = userCredential.user;
@@ -33,15 +60,21 @@ export const RegisterForm = () => {
                     }
                 })
                 .catch((error) => {
-                    console.log(error);
+                    Swal.fire({
+                        title: "Email Đã Được Sử Dụng",
+                        icon: "error",
+                    });
                 });
         }
-        else{
-            Swal.fire({
-                title: "Email Đã Được Sử Dụng",
-                icon: "error",
-            });
-        }
+    }
+
+    const handleViewPassword = (event: any) => {
+        event.preventDefault();
+        setView(!view);
+    }
+    const handleViewPassword2 = (event: any) => {
+        event.preventDefault();
+        setView2(!view2);
     }
 
     return (
@@ -87,12 +120,43 @@ export const RegisterForm = () => {
                         <div className="text-[14px] font-[600] text-white">Mật Khẩu</div>
                         <div className="text-[red]">*</div>
                     </label>
-                    <input
-                        type="password"
-                        name="password"
-                        id="password"
-                        className="px-[16px] py-[14px] rounded-[6px] outline-none w-[500px]"
-                    />
+                    <div className="flex w-[500px] px-[16px] py-[14px] rounded-[6px] bg-white">
+                        <input
+                            type={view ? "text" : "password"}
+                            name="password"
+                            id="password"
+                            className="outline-none w-full"
+                        />
+                        <button
+                            type="button"
+                            onClick={handleViewPassword}
+                        >
+                            {view ? <FaRegEyeSlash /> : <FaRegEye />}
+                        </button>
+                    </div>
+                </div>
+                <div className="mb-[15px]">
+                    <label
+                        className="flex gap-[5px] mb-[5px]"
+                        htmlFor="confirmPass"
+                    >
+                        <div className="text-[14px] font-[600] text-white">Xác Nhận Mật Khẩu</div>
+                        <div className="text-[red]">*</div>
+                    </label>
+                    <div className="flex w-[500px] px-[16px] py-[14px] rounded-[6px] bg-white">
+                        <input
+                            type={view2 ? "text" : "password"}
+                            name="confirmPass"
+                            id="confirmPass"
+                            className="outline-none w-full"
+                        />
+                        <button
+                            type="button"
+                            onClick={handleViewPassword2}
+                        >
+                            {view2 ? <FaRegEyeSlash /> : <FaRegEye />}
+                        </button>
+                    </div>
                 </div>
                 <button className="bg-[#00ADEF] w-[500px] text-[white] text-[16px] font-[700] rounded-[6px] py-[14px]">Đăng nhập</button>
                 <div className="mt-[10px] flex text-white gap-[8px] justify-end items-center">
