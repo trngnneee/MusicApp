029e86cdeb79e11974d67c0148c8ad810212009b
trngnneee <dtnguyen23@clc.fitus.edu.vn>
2025-01-09 11:18:37 +0700

    Fixed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
-import type { NextConfig } from "next";
+// next.config.ts
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: process.env.NODE_ENV === 'development'
+              ? 'http://localhost:3000'
+              : 'https://music-app-sigma-eight.vercel.app/'
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,HEAD,PUT,PATCH,POST,DELETE'
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization'
+          }
+        ]
+      }
+    ]
+  }
+}
 
-export default nextConfig;
+export default nextConfig
