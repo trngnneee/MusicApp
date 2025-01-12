b28333567353b48e894e459c44240f7febc35a2c
trngnneee <dtnguyen23@clc.fitus.edu.vn>
2025-01-12 13:16:19 +0700

    Fixed bug

--- src/app/components/Button/MenuListButton.tsx
+++ src/app/components/Button/MenuListButton.tsx
@@ -4,8 +4,6 @@ import { useState } from "react";
 import { HiOutlineViewList } from "react-icons/hi";
 
 export const MenuListButton = () => {
-    const [active, setActive] = useState(false);
-    
     const handleClick = () => {
         const siderElement = document.querySelector(".sider");
         const overlayElement = document.querySelector(".overlay");
