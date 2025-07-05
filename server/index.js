c238f493b6fcd4d01a63e66796d74837b60ff3ab
trngnneee <dtn06052005@gmail.com>
2025-07-05 15:43:19 +0700

    Doi ten mien

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const port = 8000
 mongoose.connect(process.env.DATABASE);
 
 app.use(cors({
-  origin: 'https://musicapp-di4d.onrender.com',
+  origin: 'https://music-app-nine-fawn.vercel.app',
   methods: ["GET", "POST", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
